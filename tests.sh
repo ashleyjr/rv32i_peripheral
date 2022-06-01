23cc70cad9e4612b4cbf3493dede016fac42b99f
Ashley J. Robinson <ashley181291@gmail.com>
2022-06-01 09:03:13 +0100

    change test script

--- tests.sh
+++
@@ -1,3 +0,0 @@
-verilator --trace --cc ip/x_top_urt_tx/rtl/x_top_uart_tx.sv --exe ip/x_top_uart_tx/tb/x_top_uart_tx_tb.cpp 
-make -C obj_dir -f Vx_top_uart_tx.mk Vx_top_uart_tx 
-./obj_dir/Vx_top_uart_tx 
